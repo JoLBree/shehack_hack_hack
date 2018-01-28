4d736fcfa3a1ec732f1dc0628b29323303ac5b68
Tina Lee <l-tn3314@gmail.com>
2018-01-28 00:16:53 -0500

    added some facts about anxiety

--- src/scripts/aframing.js
+++ src/scripts/aframing.js
@@ -1,23 +1,31 @@
 let textObj = {
     frontText: {
         arrInd: 0,
-        arrSize: 3,
-        textArr: ["1", "2", "3"]
+        arrSize: 4,
+        textArr: ["foobar",
+        "An estimated 19.1% of U.S. adults had any anxiety disorder in the past year",
+        "An estimated 31.1% of U.S. adults experience any anxiety disorder at some time in their lives",
+        "Past year prevalence of any anxiety disorder was higher for females (23.4%) than for males"]
     },
     rightText: {
         arrInd: 0,
         arrSize: 3,
-        textArr: ["1", "2", "3"]
+        textArr: ["foobar",
+        "Among adults with any anxiety disorder, an estimated 22.8% had serious impairment, and 33.7% had moderate impairment",
+        "A majority of people with any anxiety disorder experienced mild impairment (43.5%)"]
     },
     backText: {
         arrInd: 0,
-        arrSize: 3,
-        textArr: ["1", "2", "3"]
+        arrSize: 4,
+        textArr: ["foobar", "1", "2", "3"]
     },
     leftText: {
         arrInd: 0,
-        arrSize: 3,
-        textArr: ["1", "2", "3"]
+        arrSize: 4,
+        textArr: ["foobar",
+        "An estimated 31.9% of adolescents had any anxiety disorder",
+        "Of adolescents with any anxiety disorder, an estimated 8.3% had severe impairment",
+        "The prevalence of any anxiety disorder among adolescents was higher for females (38.0%) than for males (26.1%)"]
     }
 }
 
